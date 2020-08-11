bac76ab735eb254671aea1a388b6e1c29a08887f
elton <eltondelgadomorais@gmail.com>
2020-08-11 04:32:48 -0100

    refatorando

--- src/routes.ts
+++ src/routes.ts
@@ -1,61 +1,10 @@
 import express from 'express';
-import trx from './database/connection';
-import convertHourToMinutes from './utils/convertHourToMinutes';
-import db from './database/connection';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesControllers = new ClassesController();
 
-interface scheduleItem {
-	week_day: number;
-	from: string;
-	to: string;
-}
 
-routes.post('/classes', async (req, res) => {
-	const { name, avatar, whatsapp, bio, subject, cost, schedule } = req.body;
-
-	const trx = await db.transaction();
-
-	try {
-		const insertUsersIds = await trx('users').insert({
-			name,
-			avatar,
-			whatsapp,
-			bio
-		});
-
-		const user_id = insertUsersIds[0];
-
-		const insertedClassesIds = await trx('classes').insert({
-			subject,
-			cost,
-			user_id
-		});
-
-		const class_id = insertedClassesIds[0];
-
-		const classSchedule = schedule.map((scheduleItem: scheduleItem) => {
-			return {
-				class_id,
-				week_day: scheduleItem.week_day,
-				from: convertHourToMinutes(scheduleItem.from),
-				to: convertHourToMinutes(scheduleItem.to)
-			};
-		});
-
-		await trx('class_schedule').insert(classSchedule);
-
-		await trx.commit();
-
-		return res.status(201).send();
-	} catch (error) {
-		trx.rollback();
-
-		return res.status(400).json({
-			error: 'Unexpected error while creating new class'
-		});
-	}
-
-});
+routes.post('/classes', classesControllers.create);
 
 export default routes;
